54b083f5a0aa2e90eb87130f2c6b01a9d7c08925
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2024-10-26 11:33:36 -0700

    test

--- courses.js
+++ courses.js
@@ -1,12 +1,22 @@
 const course_url = "https://csulb.instructure.com/api/v1/courses";
 
 async function Test(){
-    const response = await fetch('https://ghibli.rest/films');
-    const data = JSON.parse(response)
-    //var bleh = JSON.parse(data)
-    data.foreach((movie)=>{
-        console.log(movie)
-    });
+    var request = new XMLHttpRequest()
+
+    request.open('GET', 'https://ghibli.rest/films', true)
+    request.onload = function () {
+      // Begin accessing JSON data here
+      var data = JSON.parse(this.response)
+    
+      if (request.status >= 200 && request.status < 400) {
+        data.forEach((movie) => {
+          console.log(movie.title)
+        })
+      } else {
+        console.log('error')
+      }
+    }
+    request.send()
 }
 // fetch(course_url)
 
