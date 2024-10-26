4793c7b77fa0ad3d32c513827e25005ac9a31d0d
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2024-10-26 12:37:46 -0700

    added canvas_key

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
+canvas_key = process.env.CANVAS_KEY
 
 const app = express();
 app.use(express.json());
