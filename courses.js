6ee35436c4ec75918e8ff9796c0aa099be77744b
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2024-10-26 11:26:51 -0700

    test ghibli films api

--- courses.js
+++ courses.js
@@ -1,17 +1,26 @@
-const course_url = "https://csulb.instructure.com/api/v1/courses"
+const course_url = "https://csulb.instructure.com/api/v1/courses";
 
-fetch(course_url)
-    .then(response =>{
-        if (!response.ok){
-            throw new Error("Network response was not ok");
-        }
-        else{
-            return response.json();
-        }
-    })
-    .then(data=>{
-        console.log(JSON.stringify(data, null, 2));
-    })
-    .catch(error =>{
-        console.error('Error: ', error)
-    })
+async function Test(){
+    const response = await fetch('https://ghibli.rest/films');
+    const data = await response.json();
+    var bleh = JSON.parse(this.response)
+    bleh.foreach((movie)=>{
+        console.log(movie)
+    });
+}
+// fetch(course_url)
+
+//     .then(response =>{
+//         if (!response.ok){
+//             throw new Error("Network response was not ok");
+//         }
+//         else{
+//             return response.json();
+//         }
+//     })
+//     .then(data=>{
+//         console.log(JSON.stringify(data, null, 2));
+//     })
+//     .catch(error =>{
+//         console.error('Error: ', error)
+//     })
