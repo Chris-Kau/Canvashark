4f58779862af130e4fbfbb5344a0839e86c820ee
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2024-10-26 21:42:57 -0700

    capitalize

--- script.js
+++ script.js
@@ -190,7 +190,7 @@ function importedCanvasAssignments(){
         const description = data[i][1]
         const date = data[i][2].toLocaleString('en-US')
         const tag = "Short task"
-        const status = "todo"
+        const status = "Todo"
     
         if (title !== "") {
             const newTask = {
