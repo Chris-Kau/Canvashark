b82611e51d5a3ad9cae2d5b262d0edd438968ab1
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2024-10-26 19:04:56 -0700

    test

--- api/assignments.js
+++ api/assignments.js
@@ -1,39 +1,39 @@
-const express = require('express');
-const fetch = require('node-fetch');
-require('dotenv').config();
+// const express = require('express');
+// const fetch = require('node-fetch');
+// require('dotenv').config();
 
-const app = express();
+// const app = express();
 
-const API_TOKEN = process.env.CANVAS_KEY
-app.use(express.json());
+// const API_TOKEN = process.env.CANVAS_KEY
+// app.use(express.json());
 
-app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use((req, res, next)=>{
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
-app.get('/api/v1', async(req, res)=>{
-    try {
-        const assignments = [];
-        const get_upcoming_assignments = await fetch(`https://csulb.instructure.com/api/v1/users/self/upcoming_events`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${API_TOKEN}`,
-                'Content-Type': 'application/json'
-            },
-        });
-        const ass = await get_upcoming_assignments.json();
-        for(var i = 0; i < ass.length; i++){
-            assignments.push([ass[i]['title'], ass[i]['description'].replace(/<[^>]+>/g, '')])
-        }
-        console.log(assignments)
-        res.json(assignments);
-    } catch (error) {
-        console.error('Error fetching user profile:', error);
-    }
-});
+// app.get('/api/v1', async(req, res)=>{
+//     try {
+//         const assignments = [];
+//         const get_upcoming_assignments = await fetch(`https://csulb.instructure.com/api/v1/users/self/upcoming_events`, {
+//             method: 'GET',
+//             headers: {
+//                 'Authorization': `Bearer ${API_TOKEN}`,
+//                 'Content-Type': 'application/json'
+//             },
+//         });
+//         const ass = await get_upcoming_assignments.json();
+//         for(var i = 0; i < ass.length; i++){
+//             assignments.push([ass[i]['title'], ass[i]['description'].replace(/<[^>]+>/g, '')])
+//         }
+//         console.log(assignments)
+//         res.json(assignments);
+//     } catch (error) {
+//         console.error('Error fetching user profile:', error);
+//     }
+// });
 
-//app.listen(3000, () => console.log("Server ready on port 3000."));
+// //app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
+// module.exports = app;
--- courses.js
+++ courses.js
@@ -1,9 +1,15 @@
 async function Test() {
     try {
-        const response = await fetch('https://canvashark.vercel.app/api/v1', {
+        // const response = await fetch('https://canvashark.vercel.app/api/v1', {
+        //     method: 'GET',
+        // });
+        const response = await fetch(`https://csulb.instructure.com/api/v1/users/self/upcoming_events`, {
             method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${API_TOKEN}`,
+                'Content-Type': 'application/json'
+            },
         });
-
         if (!response.ok) {
             console.error(`Error: ${response.status} ${response.statusText}`);
             return;
