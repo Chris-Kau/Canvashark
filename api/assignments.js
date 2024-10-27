b658493400ddf9e430e224d329ded2fa2b3c4826
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2024-10-26 19:43:40 -0700

    added due date

--- api/assignments.js
+++ api/assignments.js
@@ -6,7 +6,6 @@ export default async function handler(req, res) {
             const API_TOKEN = req.query.token;
             const assignments = [];
 
-            // Fetch the assignments from Canvas API
             const get_upcoming_assignments = await fetch(`https://csulb.instructure.com/api/v1/users/self/upcoming_events`, {
                 method: 'GET',
                 headers: {
@@ -17,11 +16,11 @@ export default async function handler(req, res) {
 
             const ass = await get_upcoming_assignments.json();
             for (let i = 0; i < ass.length; i++) {
-                assignments.push([ass[i]['title'], ass[i]['description'].replace(/<[^>]+>/g, '')]);
+                assignments.push([ass[i]['title'], ass[i]['description'].replace(/<[^>]+>/g, ''), ass[i]['due_date']]);
             }
 
             console.log(assignments);
-            res.status(200).json(assignments); // Send JSON response
+            res.status(200).json(assignments);
         } catch (error) {
             console.error('Error fetching user profile:', error);
         }
